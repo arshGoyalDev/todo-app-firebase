be02b9411fa1f02aab7ecbea29d9ee7d5d5a912a
arshGoyalDev <arshgoyalDev@gmail.com>
2024-10-01 17:43:40 +0530

    add: search function

--- context/AppContext.js
+++ context/AppContext.js
@@ -4,7 +4,7 @@ import { useContext, createContext } from "react";
 
 import { realDb } from "@/config/firebase";
 
-import { set, ref, push } from "firebase/database";
+import { set, ref, push, get } from "firebase/database";
 import { useAuth } from "./AuthContext";
 import dayjs from "dayjs";
 
@@ -33,8 +33,44 @@ const AppProvider = ({ children }) => {
     }
   };
 
+  const sleep = (resolve, ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const search = async (searchValue, searchData, setSearchData) => {
+    const dbRef = ref(realDb, `${user}`);
+    const snapshot = await get(dbRef);
+
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      let fetchedData = Object.keys(data).map((id) => {
+        return {
+          ...data[id],
+          id: id,
+        };
+      });
+
+      let tempData = [];
+
+      fetchedData.map((el) => {
+        if (
+          el.taskName.toLowerCase().includes(searchValue) ||
+          el.taskName.includes(searchValue)
+        ) {
+          if (!searchData.includes(el)) {
+            tempData = [el, ...tempData];
+          }
+        }
+      });
+
+      setSearchData(tempData);
+    }
+  };
+
   return (
-    <AppContext.Provider value={{ addTask }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ addTask, search }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
